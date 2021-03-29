1c79f14b5445699ace2c89c65ac96eb097bebb41
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-03-30 02:56:27 +0300

    [feat]: added commands for broker and server

--- cli.py
+++ cli.py
@@ -1,7 +1,10 @@
 import click
 
 from data_generator.data_generator import generated_key_value_pairs
+from broker.broker import KeyValueBroker
+from server.server import KeyValueServer
 from tools.cli_tools import file_to_list, list_of_dicts_to_file
+from tools.kv_tools import validate_ip_port
 
 
 @click.group()
@@ -33,6 +36,26 @@ def create_data(n, d, m, l, k):
     generated_key_values = generated_key_value_pairs(number_of_lines=n, level_of_nesting=d, max_number_of_keys=m,
                                                      max_word_length=l, key_names=fields_list)
     list_of_dicts_to_file(generated_key_values)
+
+
+@click.option('-a', prompt=True, required=True, type=click.STRING, help='The IP address')
+@click.option('-p', prompt=True, required=True, type=click.INT, help='The port')
+@cli.command()
+def kv_server(a, p):
+    validate_ip_port(ip_address=a, port=p)
+    server = KeyValueServer(ip_address=a, port=p)
+    server.serve()
+
+
+# @click.option('-s', prompt=True, required=True, type=click.File(), help='File that indicates the servers to connect')
+# @click.option('-i', prompt=True, required=True, type=click.File(), help='File that contains data to store')
+# @click.option('-k', prompt=True, required=True, type=click.INT, help='Replication factor, how many different'
+#                                                                      'servers will have the same replicated data')
+@cli.command()
+def kv_broker():
+    servers = [('127.0.0.1', 5000), ('127.0.0.1', 5001)]
+    broker = KeyValueBroker(servers=servers, replication_factor=1)
+    broker.send_request_to_servers()
 
 
 if __name__ == '__main__':
